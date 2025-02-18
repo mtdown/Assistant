6a80e9c6e46c4c8f9fd3526e76ac01b1fd6a0a46
alcor <2336107524@qq.com>
2025-02-18 23:03:11 +0800

    Class Dog created

---
+++ Cha08/src/Test2.java
@@ -0,0 +1,7 @@
+import com.qiang.Dog;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+    }
+}
---
+++ Cha08/src/com/use/Test.java
@@ -0,0 +1,14 @@
+package com.use;//用于定义当前类所在的包,不声明就不能import
+import com.ming.Dog;
+//只能有一个public类，且名字必须和文件名一致
+public class Test {
+    public static void main(String[] args) {
+        //使用之前必须先new
+        Dog d = new Dog();
+        //继承自Object的默认用法
+        System.out.println(d);
+        //使用前可以加上前缀来做
+        com.qiang.Dog d2 = new com.qiang.Dog();
+        System.out.println(d2);
+    }
+}
